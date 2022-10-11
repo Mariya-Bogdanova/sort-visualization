c478c009e89393813731e45e3d64240203fbf992
unknown <m_bogdanova621@mail.ru>
2022-10-11 16:38:38 +0400

    Визуалтзация сортировки

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -2,23 +2,34 @@ import { useState } from "react";
 import "./styles.css";
 
 function bubbleSort(blocks) {
+  const versionList = [];
   for (let i = 0; i < blocks.length; i++) {
     for (let j = 0; j < blocks.length - i - 1; j++) {
       if (blocks[j] > blocks[j + 1]) {
         const temp = blocks[j];
         blocks[j] = blocks[j + 1];
         blocks[j + 1] = temp;
+        versionList.push(blocks.slice());
       }
     }
   }
-  return blocks;
+  return versionList;
 }
 
 function App() {
   const [blocks, setBlocks] = useState([4, 5, 2, 3, 1]);
 
   function startSort() {
-    setBlocks(bubbleSort([4, 5, 2, 3, 1]));
+    const versionList = bubbleSort(blocks);
+    (function series(i) {
+      setTimeout(() => {
+        setBlocks(versionList[i]);
+        i++;
+        if (i < versionList.length) {
+          series(i);
+        }
+      }, 250);
+    })(0);
   }
   return (
     <>
